61584e60ba0b185dd13adb4071e8ac3d56a4986e
akh384 <akh384@miami.edu>
2025-03-30 21:16:10 -0400

    new problem fixes

--- backend/server.js
+++ backend/server.js
@@ -100,7 +100,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// Start server
+// Start server herte
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
